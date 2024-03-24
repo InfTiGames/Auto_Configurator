51ffafb8dd143648846f665ec38a9343cefed1bc
InfTiGames <92449832+InfTiGames@users.noreply.github.com>
2024-03-24 14:29:18 +0100

    Initial commit

---
+++ Assets/Auto_Configurator/Scripts/Bootstrap.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Bootstrap : MonoBehaviour
+{
+    //Место для полей инициализируемых классов
+
+    private void Awake()
+    {
+        //Порядковая реализация всех необходимых классов
+
+    }
+}
